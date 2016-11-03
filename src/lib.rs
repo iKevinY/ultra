58a4ae3131b7c219b2945a9f5f406533156ad02b
Kevin Yap <me@kevinyap.ca>
2016-11-02 23:32:00 -0700

    Implement basic Rotor type with substitutions

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,39 @@
+use std::ascii::AsciiExt;
+
+struct Rotor<'a> {
+    rotor: &'a [u8]
+}
+
+impl<'a> Rotor<'a> {
+    pub fn new(rotor: &'a str) -> Rotor<'a> {
+        Rotor {
+            rotor: rotor.as_bytes(),
+        }
+    }
+
+    fn substitute(&self, c: char) -> char {
+        if !c.is_ascii() || !c.is_alphabetic() {
+            return c;
+        }
+
+        let letter = c.to_ascii_uppercase();
+        let offset = ((letter as u8) - ('A' as u8)) as usize;
+
+        self.rotor[offset] as char
+    }
+}
+
+
 #[cfg(test)]
 mod tests {
+    use super::Rotor;
+
     #[test]
-    fn it_works() {
+    fn char_substitution() {
+        let rotor = Rotor::new("XYZ");
+        assert!(rotor.substitute('A') == 'X');
+        assert!(rotor.substitute('b') == 'Y');
+        assert!(rotor.substitute('!') == '!');
+        assert!(rotor.substitute('é') == 'é');
     }
 }
