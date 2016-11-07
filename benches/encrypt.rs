948b3dc211be018724a9b7b947a9aecab9ec7431
Kevin Yap <me@kevinyap.ca>
2016-11-07 12:45:40 -0800

    Add benchmarks for encryption methods

---
+++ benches/encrypt.rs
@@ -0,0 +1,20 @@
+#![feature(test)]
+
+extern crate ultra;
+extern crate test;
+
+use test::Bencher;
+use ultra::enigma::Enigma;
+
+
+#[bench]
+fn bench_encrypt_char(b: &mut Bencher) {
+    let mut enigma = Enigma::new(1, 2, 3, 'B');
+    b.iter(|| enigma.encrypt_char('A'));
+}
+
+#[bench]
+fn bench_encrypt_msg(b: &mut Bencher) {
+    let mut enigma = Enigma::new(1, 2, 3, 'B');
+    b.iter(|| enigma.encrypt("THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG"));
+}
