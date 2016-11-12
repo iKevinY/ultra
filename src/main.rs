6841b2ce5940ac21e53da254a745e7645019ed7f
Kevin Yap <me@kevinyap.ca>
2016-11-11 19:13:58 -0800

    Add basic enciphering functionality to CLI

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,17 @@
 extern crate ultra;
 
+use std::io::{stdin, stdout, Write};
+use ultra::enigma::Enigma;
+
 fn main() {
-    println!("Hello, World War II");
+    let mut msg = String::new();
+    print!("Message to encrypt: ");
+    stdout().flush().unwrap();
+    stdin().read_line(&mut msg).unwrap();
+    let msg = msg.trim_right();
+
+    if !msg.is_empty() {
+        let mut enigma = Enigma::new("123", "AAA", "AAA", 'B', "");
+        println!("Ciphertext: {}", enigma.encrypt(msg))
+    }
 }
