941773b2718b5483652fbb13b6cef9caef8fb444
Kevin Yap <me@kevinyap.ca>
2016-11-15 19:05:13 -0800

    Add randomization option to CLI

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 #[macro_use]
 extern crate clap;
+extern crate rand;
 extern crate ultra;
+
+use rand::Rng;
 
 use ultra::enigma::Enigma;
 use ultra::decrypt::decrypt;
@@ -11,6 +14,7 @@ fn main() {
         (@setting ArgRequiredElseHelp)
         (@setting ColoredHelp)
         (@arg decrypt: --decrypt -d "Decrypt a given piece of ciphertext")
+        (@arg randomize: --randomize -R "Encrypt a message with random Enigma settings")
         (@arg ROTORS: --rotor -w +takes_value "Rotor order (default: \"123\")")
         (@arg KEY: --key -k +takes_value "Key settings (default: \"AAA\")")
         (@arg RING: --ring -r +takes_value "Ring settings (default: \"AAA\")")
@@ -22,17 +26,46 @@ fn main() {
     let msg = matches.value_of("MESSAGE").unwrap();
 
     if matches.is_present("decrypt") {
-        let (plaintext, key, ring, rotor) = decrypt(msg);
+        let (plaintext, key, ring, rotors) = decrypt(msg);
         println!("{}", plaintext);
-        println!("(Key Setting: {}, Ring Setting: {}, Rotors: {})", key, ring, rotor);
-        return;
+        println!("(Key Setting: {}, Ring Setting: {}, Rotors: {})", key, ring, rotors);
     }
 
-    let rotors = matches.value_of("ROTORS").unwrap_or("123");
-    let key = matches.value_of("KEY").unwrap_or("AAA");
-    let ring = matches.value_of("RING").unwrap_or("AAA");
-    let plugboard = matches.value_of("PLUGBOARD").unwrap_or("");
+    else if matches.is_present("randomize") {
+        let mut rng = rand::thread_rng();
 
-    let mut enigma = Enigma::new(rotors, key, ring, 'B', plugboard);
-    println!("{}", enigma.encrypt(msg))
+        let rotors: String = {
+            let mut rotor_pool: Vec<char> = "12345".chars().collect();
+            let mut rotors: Vec<char> = Vec::with_capacity(3);
+
+            for _ in 0..3 {
+                let len = rotor_pool.len();
+                rotors.push(rotor_pool.remove(rng.gen_range(0, len)));
+            }
+
+            rotors.into_iter().collect()
+        };
+
+        let mut key = String::with_capacity(3);
+        let mut ring = String::with_capacity(3);
+
+        for _ in 0..3 {
+            key.push(rng.gen_range(b'A', b'Z' + 1) as char);
+            ring.push(rng.gen_range(b'A', b'Z' + 1) as char);
+        }
+
+        let mut enigma = Enigma::new(&rotors, &key, &ring, 'B', "");
+        println!("{}", enigma.encrypt(msg));
+        println!("(Key Setting: {}, Ring Setting: {}, Rotors: {})", &key, &ring, &rotors);
+    }
+
+    else {
+        let rotors = matches.value_of("ROTORS").unwrap_or("123");
+        let key = matches.value_of("KEY").unwrap_or("AAA");
+        let ring = matches.value_of("RING").unwrap_or("AAA");
+        let plugboard = matches.value_of("PLUGBOARD").unwrap_or("");
+
+        let mut enigma = Enigma::new(rotors, key, ring, 'B', plugboard);
+        println!("{}", enigma.encrypt(msg));
+    }
 }
