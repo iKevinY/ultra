bec1df46d6b2c17700af79d0965406ccfbe71410
Kevin Yap <me@kevinyap.ca>
2016-11-18 13:16:24 -0800

    Add test for Enigma turnover points

--- tests/encrypt.rs
+++ tests/encrypt.rs
@@ -7,3 +7,9 @@ fn expected_ciphertext() {
     let mut enigma = Enigma::new("123", "BAT", "HTU", 'B', "");
     assert_eq!(enigma.encrypt("THEQUICKBROWNFOX"), "USSXBXPNRLBSTKQR");
 }
+
+#[test]
+fn turnover_points() {
+	let mut enigma = Enigma::new("123", "AAA", "ADU", 'B', "");
+	assert_eq!(enigma.encrypt("THEQUICKBROWNFOX"), "ACGXKHKYCBVQZMJM");
+}
