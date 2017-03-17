7e671cf8d480e877ba4cf256ffc0ab7f4d3cb85d
Kevin Yap <me@kevinyap.ca>
2017-03-17 00:52:41 -0700

    Refactor ciphertext decryption
    Construct vectors of strings for rotor and key/ring permutations
    statically and iterate over indices instead of creating the strings
    during iteration. This appears to not impact performance, but is
    much more readable.

--- src/decrypt.rs
+++ src/decrypt.rs
@@ -20,6 +20,21 @@ lazy_static! {
 
         qgrams
     };
+
+    static ref ALPHAS: Vec<String> = {
+        // Construct the vector ["AAA", "AAB", ..., "ZZY", "ZZZ"]
+        iproduct!(b'A'..b'[', b'A'..b'[', b'A'..b'[')
+            .map(|(a, b, c)| String::from_utf8(vec![a, b, c]).unwrap())
+            .collect()
+    };
+
+    static ref ROTORS: Vec<String> = {
+        // Create all permutations of rotors 1 through 5
+        iproduct!(b'1'..b'6', b'1'..b'6', b'1'..b'6')
+            .filter(|&(r1, r2, r3)| (r1 != r2) && (r1 != r3) && (r2 != r3))
+            .map(|(r1, r2, r3)| String::from_utf8(vec![r1, r2, r3]).unwrap())
+            .collect()
+    };
 }
 
 
@@ -27,39 +42,36 @@ lazy_static! {
 /// configurations, returning the tuple `(plaintext, rotor, key, ring)`
 /// corresponding to the most probable plaintext.
 pub fn decrypt(msg: &str) -> (String, String, String, String) {
-    // Rotor and key settings (60*26^3 == 1,054,560 decryptions)
-    let (_, k1, best_rotor) = iproduct!(
-            b'1'..b'6', b'1'..b'6', b'1'..b'6',
-            b'A'..b'[', b'A'..b'[', b'A'..b'['
-        ).collect::<Vec<_>>()
-        .par_iter()
-        .filter(|&&(r1, r2, r3, _, _, _)| (r1 != r2) && (r1 != r3) && (r2 != r3))
-        .map(|&(r1, r2, r3, k1, k2, k3)| {
-            let rotor = String::from_utf8(vec![r1, r2, r3]).unwrap();
-            let key = String::from_utf8(vec![k1, k2, k3]).unwrap();
-
-            let mut enigma = Enigma::new(&rotor, &key, "AAA", 'B', "");
+    // Try all rotor/key permutations (60*26^3 == 1,054,560 decryptions)
+    let (rotor, key) = iproduct!(ROTORS.iter(), ALPHAS.iter())
+        .collect::<Vec<_>>()
+        .into_par_iter()
+        .max_by_key(|&(rotor, key)| {
+            let mut enigma = Enigma::new(rotor, key, "AAA", 'B', "");
             let plaintext = enigma.encrypt(msg);
-            let score = qgram_score(&plaintext);
-            (OrderedFloat(score), k1, rotor)
-        }).max().unwrap();
+            OrderedFloat(qgram_score(&plaintext))
+        }).unwrap();
 
-    // Key and ring settings (26^4 == 456,976 decryptions)
-    let (_, best_msg, best_key, best_ring) = iproduct!(
-            b'A'..b'[', b'A'..b'[', b'A'..b'[', b'A'..b'['
-        ).collect::<Vec<_>>()
+    // Keep the best rotor configuration found previously, and use the same
+    // key setting for the first (slow) rotor. The ring setting for the first
+    // rotor doesn't matter, so fix it to 'A', and try all key/ring settings
+    // for the remaining two rotors (26^4 == 456,976 decryptions)
+    let key_offset = key.chars().nth(0).unwrap().index() * 676;
+
+    let (_, msg, key, ring) = iproduct!(key_offset..(key_offset + 676), 0..676)
+        .collect::<Vec<_>>()
         .par_iter()
-        .map(|&(k2, k3, r2, r3)| {
-            let key = String::from_utf8(vec![k1, k2, k3]).unwrap();
-            let ring = String::from_utf8(vec![b'A', r2, r3]).unwrap();
+        .map(|&(key_index, ring_index)| {
+            let key = &ALPHAS[key_index];
+            let ring = &ALPHAS[ring_index];
 
-            let mut enigma = Enigma::new(&best_rotor, &key, &ring, 'B', "");
+            let mut enigma = Enigma::new(rotor, key, ring, 'B', "");
             let plaintext = enigma.encrypt(msg);
             let score = qgram_score(&plaintext);
             (OrderedFloat(score), plaintext, key, ring)
         }).max().unwrap();
 
-    (best_msg, best_rotor, best_key, best_ring)
+    (msg, rotor.clone(), key.clone(), ring.clone())
 }
 
 
