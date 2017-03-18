86d3d6b7795adb3b1afa702a342b6d1642616e37
Kevin Yap <me@kevinyap.ca>
2017-03-17 22:20:34 -0700

    Convert indentation to spaces

--- tests/decrypt.rs
+++ tests/decrypt.rs
@@ -4,7 +4,7 @@ use ultra::decrypt;
 
 #[test]
 fn basic_decryption() {
-	let expected = "THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG";
+    let expected = "THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG";
     let ciphertext = "NTZ NTQLZ JMWLL ART BBNOW WZQK KEQ IEVK LZO";
     let (plaintext, rotor, _, _) = decrypt(ciphertext);
 
