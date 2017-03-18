8795636e2bf74fd7edaa69e1760f5ed590c75752
Kevin Yap <me@kevinyap.ca>
2017-03-17 18:39:28 -0700

    Add integration test for decryption function
    Increase development profile opt-level to ensure this test runs quickly.

---
+++ tests/decrypt.rs
@@ -0,0 +1,13 @@
+extern crate ultra;
+
+use ultra::decrypt;
+
+#[test]
+fn basic_decryption() {
+	let expected = "THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG";
+    let ciphertext = "NTZ NTQLZ JMWLL ART BBNOW WZQK KEQ IEVK LZO";
+    let (plaintext, rotor, _, _) = decrypt(ciphertext);
+
+    assert_eq!(plaintext, expected);
+    assert_eq!(rotor, "142");
+}
