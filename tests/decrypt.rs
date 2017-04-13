iKevinY/ultra

commit a3360f115f26504ced689ee1771d8a359e392f53

Optimize ciphertext decryption algorithm
After the best rotor and key configuration is chosen from the parallel
step, there is no need to try new key settings. This reduces the number
of decryptions performed in the second step from 26^4 to 26^2, which is
small enough that it seems reasonable to not parallelize it.

src/decrypt.rs
@@ -1,3 +1,4 @@
+use itertools::Itertools;
 use ordered_float::OrderedFloat;
 use rayon::prelude::*;
 
@@ -42,7 +43,7 @@ lazy_static! {
 /// configurations, returning the tuple `(plaintext, rotor, key, ring)`
 /// corresponding to the most probable plaintext.
 pub fn decrypt(msg: &str) -> (String, String, String, String) {
-    // Try all rotor/key permutations (60*26^3 == 1,054,560 decryptions)
+    // Try all rotor/key permutations (60*26^3 == 1,054,560 decryptions).
     let (rotor, key) = iproduct!(ROTORS.iter(), ALPHAS.iter())
         .collect::<Vec<_>>()
         .into_par_iter()
@@ -52,24 +53,21 @@ pub fn decrypt(msg: &str) -> (String, String, String, String) {
             OrderedFloat(qgram_score(&plaintext))
         }).unwrap();
 
-    // Keep the best rotor configuration found previously, and use the same
-    // key setting for the first (slow) rotor. The ring setting for the first
-    // rotor doesn't matter, so fix it to 'A', and try all key/ring settings
-    // for the remaining two rotors (26^4 == 456,976 decryptions)
-    let key_offset = key.chars().nth(0).unwrap().index() * 676;
+    // Use the best rotor/key settings found previously, and iterate through
+    // ring settings for the latter two rotors (the ring setting for the slow
+    // rotor doesn't matter, so fix it to 'A'), shifting the key setting to
+    // match the corresponding ring setting (26^2 == 676 decryptions).
+    let (k1, k2, k3) = key.chars().map(|c| c.index()).next_tuple().unwrap();
 
-    let (_, msg, key, ring) = iproduct!(key_offset..(key_offset + 676), 0..676)
-        .collect::<Vec<_>>()
-        .par_iter()
-        .map(|&(key_index, ring_index)| {
-            let key = &ALPHAS[key_index];
-            let ring = &ALPHAS[ring_index];
+    let (_, msg, key, ring) = iproduct!(0..26, 0..26).map(|(r2, r3)| {
+        let key = &ALPHAS[k1 * 676 + ((k2 + r2) % 26) * 26 + (k3 + r3) % 26];
+        let ring = &ALPHAS[r2 * 26 + r3];
 
-            let mut enigma = Enigma::new(rotor, key, ring, 'B', "");
-            let plaintext = enigma.encrypt(msg);
-            let score = qgram_score(&plaintext);
-            (OrderedFloat(score), plaintext, key, ring)
-        }).max().unwrap();
+        let mut enigma = Enigma::new(rotor, key, ring, 'B', "");
+        let plaintext = enigma.encrypt(msg);
+        let score = qgram_score(&plaintext);
+        (OrderedFloat(score), plaintext, key, ring)
+    }).max().unwrap();
 
     (msg, rotor.clone(), key.clone(), ring.clone())
 }

commit 1e48e6088dccbf151bc72c65073572e939b1cc14

Add additional decryption test
This test prevents the errant decryption caused by a3360f1.

tests/decrypt.rs
@@ -2,12 +2,25 @@ extern crate ultra;
 
 use ultra::decrypt;
 
-#[test]
-fn basic_decryption() {
-    let expected = "THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG";
-    let ciphertext = "NTZ NTQLZ JMWLL ART BBNOW WZQK KEQ IEVK LZO";
-    let (plaintext, rotor, _, _) = decrypt(ciphertext);
+macro_rules! decrypt_test {
+    ($expected:tt, $ciphertext:tt) => {
+        assert_eq!(decrypt($ciphertext).0, $expected);
+    }
+}
 
-    assert_eq!(plaintext, expected);
-    assert_eq!(rotor, "142");
+
+#[test]
+fn short_decryption() {
+    decrypt_test!(
+        "THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG",
+        "NTZ NTQLZ JMWLL ART BBNOW WZQK KEQ IEVK LZO"
+    )
+}
+
+#[test]
+fn longer_decryption() {
+    decrypt_test!(
+        "A FEW MILES SOUTH OF SOLEDAD, THE SALINAS RIVER DROPS IN CLOSE TO THE HILLSIDE BANK AND RUNS DEEP AND GREEN. THE WATER IS WARM TOO, FOR IT HAS SLIPPED TWINKLING OVER THE YELLOW SANDS IN THE SUNLIGHT BEFORE REACHING THE NARROW POOL.",
+        "Q OCB ODJKE OFZDD BQ YRIRNDW, QZD BOOJHXT PUULE MKCBC FO UAIYG IL ITF ALSUDRGO IKEA LWG SWPA HACL ZDP PDSNQ. WTH OSFWJ TU DKHV VUI, KTM WZ GOR TJDKVAG FXYMZKFWE ENRH BXI PRZZWM GYIGH JL WDM TYSTZLRP EPABZL ZACPUPJY UPU TEQLCX UZBM."
+    )
 }